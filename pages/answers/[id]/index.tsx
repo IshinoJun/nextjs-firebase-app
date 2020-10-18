4eab572b8ce24dcd058030b313ca1a33d3df2209
JunIshino <jun.ishino@kepple.jp>
2020-10-18 20:40:02 +0900

    ogp対応

--- pages/answers/[id]/index.tsx
+++ pages/answers/[id]/index.tsx
@@ -10,6 +10,8 @@ type Props = {
 }
 
 export default function AnswersShow(props: Props) {
+  const ogpImageUrl = `${process.env.NEXT_PUBLIC_WEB_URL}/api/answers/${props.answer.id}/ogp`
+
   const getDescription = (answer: Answer) => {
     const body = answer.body.trim().replace(/[ \r\n]/g, '')
     if (body.length < 140) {
@@ -23,6 +25,9 @@ export default function AnswersShow(props: Props) {
   return (
     <Layout>
       <Head>
+        <meta property="og:image" key="ogImage" content={ogpImageUrl} />
+        <meta name="twitter:card" key="twitterCard" content="summary_large_image" />
+        <meta name="twitter:image" key="twitterImage" content={ogpImageUrl} />
         <meta name="description" key="description" content={description} />
         <meta
           property="og:description"
